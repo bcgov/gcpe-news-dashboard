c7f8f5b4613dc843361ac6126f583081ec503eb9
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2018-10-31 10:51:18 -0700

    get changes from remote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,26 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from  './core/navmenu/navmenu.component';
 import { ActivityListComponent } from './activities/activity-list/activity-list.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { ActivitiesForecastListComponent } from './activities/activities-forecast-list/activities-forecast-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'activity-list', component: ActivityListComponent },
+  { path: 'activity-forecast-list', component: ActivitiesForecastListComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     ActivityListComponent,
-    FooterComponent
+    FooterComponent,
+    ActivitiesForecastListComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }),
     AppRoutingModule
   ],
   providers: [],
