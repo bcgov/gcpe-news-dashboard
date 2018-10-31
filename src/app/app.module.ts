731a747975b22cab0062d44182abcdce87e66983
Bruno Calvignac <bruno.calvignac@gov.bc.ca>
2018-10-31 10:26:51 -0700

    Hooked to dev.api.news.gov.bc.ca instead of mocking the news data Renamed Activity to Post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from  './core/navmenu/navmenu.component';
-import { ActivityListComponent } from './activities/activity-list/activity-list.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    ActivityListComponent,
+    PostListComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     NgbModule.forRoot(),
     AppRoutingModule
   ],
