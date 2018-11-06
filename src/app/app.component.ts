b261385b70c048a46ddac6bdd3f997cddb308b8d
André Lashley <andre.lashley@gmail.com>
2018-11-06 15:38:06 -0800

    Implement keycloak sign in and sign out in the dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import {
+  OAuthService,
+  JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './auth.config';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +14,10 @@ import { Router } from '@angular/router';
 
 export class AppComponent {
   title = 'BC Gov News';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
 }
--- src/app/core/navmenu/navmenu.component.ts
+++ src/app/core/navmenu/navmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'news-dashboard-navmenu',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
   isCollapsed = true;
-  ngOnInit() {
-    
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  isLoggedIn() {
+    return this.authService.loggedIn();
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logOut() {
+    this.authService.logOut();
   }
 }
