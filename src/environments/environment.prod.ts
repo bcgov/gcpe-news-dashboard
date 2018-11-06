6c9f3a420dd42034ff85dd3a65eeb1cbac3c07ad
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2018-11-06 11:14:52 -0800

    temporarily set the prod api url to run the openshift pipeline

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://dev.api.news.gov.bc.ca/"
+  apiUrl: "https://dev.api.news.gov.bc.ca/"   // temporarily set the prod api url to run the openshift pipeline
 };
