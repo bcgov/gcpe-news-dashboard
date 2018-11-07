03943034853b77849dac363d5d6179df37013182
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2018-11-07 11:32:31 -0800

    Dev refactor styles and routing (#13)
    * fixed the prod environment variable to run openshift pipeline
    
    * temporarily set the prod api url to run the openshift pipeline
    
    * remove duplicated base tag
    
    * move the css with the related component and move the router to the routing module
    
    * update the package file
    
    * fixed the unit test after implementing OAuth

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Post } from '../../shared/post';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../services/api.service'
