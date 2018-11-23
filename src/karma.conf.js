224496d632dd29a55522d02a6b2b46ad3a8edfad
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2018-11-23 13:44:51 -0800

    test openshift config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -36,7 +36,21 @@ module.exports = function (config) {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
-      }
+      },
+      ChromeCI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--disable-gpu']
+      },
+      'ChromeHeadlessCustom': {
+        base: 'ChromiumHeadless',
+        flags: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '--disable-translate',
+            '--disable-extensions'
+        ]
+    }
     },
     singleRun: false
   });
