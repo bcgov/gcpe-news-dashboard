aa263390f4eb01a67822acef8d1b04be54bcf6a5
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2018-11-26 09:51:02 -0800

    test e2e config

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -8,7 +8,8 @@ const puppeteer = require('puppeteer');
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-gpu'],
+    //args: ['--headless', '--no-sandbox', '--disable-gpu'],
+    args: ['--headless', '--no-sandbox'],
     binary: puppeteer.executablePath()
   }
 };
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('');
+    expect(page.getParagraphText()).toEqual('BC Gov News');
   });
 });
