bca690b7349a35e06b5b2b2ef4f2f35a0777d04d
Brian Richter <brianorwhatever@gmail.com>
2018-11-29 13:40:30 -0800

    themes of the week works

--- src/app/themes/theme-list/theme-list.component.ts
+++ src/app/themes/theme-list/theme-list.component.ts
@@ -10,31 +10,12 @@ import { ApiService } from '../../services/api.service';
 })
 export class ThemeListComponent implements OnInit {
   themes: Theme[];
-  selectedThemes: Theme[];
 
   constructor(private router: Router, private  apiService:  ApiService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.themes = data['themelist'];
-      this.selectedThemes = data['themelist'];
-      // mockup data for the theme list
-      // this.themes.map(function (item, index, list) {
-      //   if (index%2 == 0)
-      //   {
-      //     item.type = 'Published';
-      //   }
-      //   else
-      //   {
-      //     item.type = 'Drafts';
-      //   }
-      // });
     });
-
-    // this.route.queryParams.subscribe((queryParams:any) => {
-    //   this.selectedThemes = this.themes.filter( s => s.type === queryParams.type );
-    //  });
-
   }
-
 }
