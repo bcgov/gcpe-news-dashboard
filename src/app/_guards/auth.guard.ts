85a566c3d02041cde45e811f97995ca885098320
Lashley <ANLASHLE@BCGOV>
2019-01-09 11:16:51 -0800

    Cleanup

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(
+        private authService: AuthService,
+        private router: Router) {}
+
+    canActivate(next: ActivatedRouteSnapshot): boolean {
+        const roles = next.data['roles'] as Array<string>;
+        if (roles) {
+            const match = this.authService.roleMatch(roles);
+            if (match) {
+                return true;
+            } else {
+                this.router.navigate(['last-7-day-post-list']);
+                window.alert('You are not authorized to access this area!');
+            }
+        }
+
+        if (this.authService.loggedIn()) {
+            return true;
+        }
+
+        window.alert('You are not logged in!');
+        this.router.navigate(['last-7-day-post-list']);
+        return false;
+    }
+}
