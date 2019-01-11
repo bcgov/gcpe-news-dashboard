bc7432c56358cb55d662970dcd5617784bba6488
André Lashley <andre.lashley@gmail.com>
2019-01-11 11:57:33 -0800

    Move authorization implementation to the dropdown menu (#82)
    * Implement keycloak sign in and sign out in the dashboard
    
    * Add Sonarqube JenkinsFile to the dashboard
    
    * Make changes to openshift config
    
    * Hotfix
    
    * Add sonar-runner folder
    
    * Add configuration in Jenkinsfile for sonarqube scanning
    
    * Setup snyk infrastructure
    
    * Hotfix
    
    * Remove Dotnet-Snyk and rework configuration
    
    * Config change
    
    * Install snyk in frontend-snyk Dockerfile
    
    * snyk Dockerfile config change
    
    * Snyk test config
    
    * Add items for sonarqube scan
    
    * Remove the sonar-runner directory
    
    * Rework develop full pipeline
    
    * Rework Jenkinsfiles
    
    * Remove console log statements
    
    * Include change to package lock file
    
    * - Add tests and initial implementation of authorization directive
    - Add dependencies to existing tests so that they still pass
    - lint some app files using tslint plugin
    
    * Cleanup
    
    * Cleanup package-lock.json
    
    * Fix security vulnerability in webpack-dev-server
    
    * Lint all Typescript files
    
    * Clean up suggested in code review
    
    * Make changes from code review
    
    * Move authorization implementation to the dropdown menu

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -10,17 +10,7 @@ export class AuthGuard implements CanActivate {
         private authService: AuthService,
         private router: Router) {}
 
-    canActivate(next: ActivatedRouteSnapshot): boolean {
-        const roles = next.data['roles'] as Array<string>;
-        if (roles) {
-            const match = this.authService.roleMatch(roles);
-            if (match) {
-                return true;
-            } else {
-                this.router.navigate(['last-7-day-post-list']);
-            }
-        }
-
+    canActivate(): boolean {
         if (this.authService.loggedIn()) {
             return true;
         }
