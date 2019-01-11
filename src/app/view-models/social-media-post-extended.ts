d8418795828d51f5670fcc789c5c7f241d9c6dd8
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2019-01-11 13:19:02 -0800

    fixed the social media post overlapping issue

--- src/app/view-models/social-media-post-extended.ts
+++ src/app/view-models/social-media-post-extended.ts
@@ -14,24 +14,34 @@ export class SocialMediaPostExtended implements SocialMediaPost {
         this.sortOrder = post.sortOrder;
         this.timestamp = post.timestamp;
         this.mediaType = this.getMediaType(post.url);
+        this.postType = this.getPostType(post.url);
     }
 
-    getMediaType(url:string): string{
+    getMediaType(url: string): string {
         let type: string;
         type = '';
-        if (url.indexOf('facebook')>=0)
-        {
+        if (url.indexOf('facebook') >= 0) {
             type = 'Facebook';
-        }
-        else if (url.indexOf('twitter')>=0)
-        {
+        } else if (url.indexOf('twitter') >= 0 ) {
             type = 'Twitter';
-        }
-        else if (url.indexOf('instagram') >=0)
-        {
+        } else if (url.indexOf('instagram') >= 0) {
             type = 'Instagram';
         }
         return type;
     }
+
+    getPostType(url: string): string {
+        let type: string;
+        type = '';
+        if (url.indexOf('facebook') >= 0) {
+            if (url.indexOf('post') >= 0) {
+                type = 'fb-post';
+            } else if (url.indexOf('video') >= 0) {
+                type = 'fb-video';
+            }
+        }
+        return type;
+    }
+
 
 }
