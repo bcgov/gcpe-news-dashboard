e0e640993eeadff0488a6729e38dad340d7e8a3b
Brian Richter <brianorwhatever@gmail.com>
2019-01-18 16:27:44 -0800

    moved backup config

--- src/app/app-config.service.ts
+++ src/app/app-config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { config } from '../environments/backupConfig';
+import { config } from './backup.config';
 
 @Injectable()
 export class AppConfigService {
