a4fdedd4e6665a0bbf9368701c0e40d1fd34d819
Brian Richter <brianorwhatever@gmail.com>
2019-02-05 13:24:52 -0800

    added tests for has role directive

--- src/app/_directives/hasRole.directive.ts
+++ src/app/_directives/hasRole.directive.ts
@@ -6,7 +6,6 @@ import { AuthService } from '../services/auth.service';
 })
 export class HasRoleDirective implements OnInit {
   @Input() appHasRole: string[];
-  isVisible = false;
 
   constructor(
     private viewContainerRef: ViewContainerRef,
@@ -14,22 +13,10 @@ export class HasRoleDirective implements OnInit {
     private authService: AuthService) {}
 
   ngOnInit() {
-    const userRoles = this.authService.identityClaims['user_roles'] as Array<String>;
-    // if the user has no roles, do not display the element in question
-    if (!userRoles) {
+    if (this.authService.roleMatch(this.appHasRole)) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
       this.viewContainerRef.clear();
     }
-
-    // if user has required role then render the element
-    if (this.authService.roleMatch(this.appHasRole)) {
-      if (!this.isVisible) {
-        this.isVisible = true;
-        this.viewContainerRef.createEmbeddedView(this.templateRef);
-      } else {
-        this.isVisible = false;
-        this.viewContainerRef.clear();
-      }
-    }
   }
-
 }
