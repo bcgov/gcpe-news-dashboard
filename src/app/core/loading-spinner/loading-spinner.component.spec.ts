b5f87aa3e1a67ecee0f8bacc941ec6dd758f46b4
Brian Richter <brianorwhatever@gmail.com>
2019-02-13 15:11:03 -0800

    tests for loading spinner

--- src/app/core/loading-spinner/loading-spinner.component.spec.ts
+++ src/app/core/loading-spinner/loading-spinner.component.spec.ts
@@ -1,7 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { LoadingSpinnerComponent } from './loading-spinner.component';
 
@@ -24,5 +23,16 @@ describe('LoadingSpinnerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show when loading', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#loading-spinner'))).toBeTruthy();
+  });
+
+  it('should not show when not loading', () => {
+    component.isLoading = false;
+    expect(fixture.debugElement.query(By.css('#loading-spinner'))).toBeFalsy();
   });
 });
