e2d4db0932c4c054796383a26c8a77df42863d29
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2019-02-19 13:11:57 -0800

    Update themes-of-week.po.ts

--- e2e/src/themes/themes-of-week.po.ts
+++ e2e/src/themes/themes-of-week.po.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { browser, element, by } from 'protractor';
 
 export class ThemesOfWeekPage {
@@ -23,30 +22,3 @@ export class ThemesOfWeekPage {
     }
     */
 }
-||||||| merged common ancestors
-=======
-import { browser, element, by } from 'protractor';
-
-export class ThemesOfWeekPage {
-    navigateTo() {
-        return browser.get('/themes-of-the-week');
-    }
-
-    getUrl() {
-        return browser.getCurrentUrl();
-    }
-/*comment all integration test to have the pipeline running
-    getHeadingText() {
-        return element(by.css('app-hq-dashboard-sub-menu .dashboard-sub-menu-container h2')).getText();
-    }
-
-    getSelectedSubNavItem() {
-        return element(by.css('app-hq-dashboard-sub-menu .dashboard-sub-menu-container li.list-inline-item .active')).getText();
-    }
-
-    getThemeList() {
-        return element(by.css('#theme-list-container #theme-list'));
-    }
-    */
-}
->>>>>>> ab01ff450d2a7e023069259ba0da7e1ea1256db4
