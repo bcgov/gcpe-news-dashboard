db18b31175c8adb9efc60acdfc44d2dfbebb6e2c
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2019-02-19 13:11:38 -0800

    Update themes-of-week.e2e-spec.ts

--- e2e/src/themes/themes-of-week.e2e-spec.ts
+++ e2e/src/themes/themes-of-week.e2e-spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ThemesOfWeekPage } from './themes-of-week.po';
 
 describe('Themes of Week Page', () => {
@@ -22,29 +21,3 @@ describe('Themes of Week Page', () => {
   });
   */
 });
-||||||| merged common ancestors
-=======
-import { ThemesOfWeekPage } from './themes-of-week.po';
-
-describe('Themes of Week Page', () => {
-  let page: ThemesOfWeekPage;
-
-  beforeAll(() => {
-    page = new ThemesOfWeekPage();
-    page.navigateTo();
-  });
-/* comment integration tests to have the pipeline running
-  it('should display header text', () => {
-    expect(page.getHeadingText()).toEqual('HQ Dashboard');
-  });
-
-  it('should have proper sub nav item selected', () => {
-    expect(page.getSelectedSubNavItem()).toEqual('THEMES OF THE WEEK');
-  });
-
-  it('should have theme list', () => {
-    expect(page.getThemeList()).toBeTruthy();
-  });
-  */
-});
->>>>>>> ab01ff450d2a7e023069259ba0da7e1ea1256db4
