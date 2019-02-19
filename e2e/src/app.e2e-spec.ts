93eabd0e6a3570e7a0723ce91be115b16ccf549b
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2019-02-19 13:11:07 -0800

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { AppPage } from './app.po';
 import { browser } from 'protractor';
 
@@ -16,23 +15,4 @@ describe('workspace-project App', () => {
     expect(page.getParagraphText()).toEqual('BC Gov News');
   });
 });
-||||||| merged common ancestors
-=======
-import { AppPage } from './app.po';
-import { browser } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-
-    browser.waitForAngularEnabled(false);
-    page = new AppPage();
-  });
-
-  it('should display app title', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('BC Gov News');
-  });
-});
->>>>>>> ab01ff450d2a7e023069259ba0da7e1ea1256db4
