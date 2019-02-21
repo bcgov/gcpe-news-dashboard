e0e6c5448d7e66a669f26e0d4c42fa30a640ad82
Brian Richter <brianorwhatever@gmail.com>
2019-02-21 14:25:25 -0800

    use dynamic origin for redirects

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,9 +1,11 @@
+var origin = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port : '');
+
 export const authConfig = {
   clientID: "974b09f9-8093-476b-a795-9e6b4e317f6b",
   authority: "https://login.microsoftonline.com/bcgov.onmicrosoft.com/",
   validateAuthority: true,
-  redirectUri: "http://localhost:4200/",
+  redirectUri: origin,
   cacheLocation: "localStorage",
-  postLogoutRedirectUri: "http://localhost:4200/",
+  postLogoutRedirectUri: origin,
   navigateToLoginRequestUrl: true
 };
