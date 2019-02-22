7d17ef1942374cdde144078e215371143793f150
Brian Richter <brianorwhatever@gmail.com>
2019-02-21 16:54:47 -0800

    remove redirect uri entirely

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,10 +1,7 @@
-var origin = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '');
-
 export const authConfig = {
   clientID: "974b09f9-8093-476b-a795-9e6b4e317f6b",
   authority: "https://login.microsoftonline.com/bcgov.onmicrosoft.com/",
   validateAuthority: true,
-  redirectUri: origin,
   cacheLocation: "localStorage",
   postLogoutRedirectUri: origin,
   navigateToLoginRequestUrl: true
