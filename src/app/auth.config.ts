3924e0b1598f618e35e64a743f82f860bde6d4f9
Brian Richter <brianorwhatever@gmail.com>
2019-02-21 16:16:16 -0800

    fixed redirect url

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,4 +1,4 @@
-var origin = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port : '');
+var origin = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '');
 
 export const authConfig = {
   clientID: "974b09f9-8093-476b-a795-9e6b4e317f6b",
