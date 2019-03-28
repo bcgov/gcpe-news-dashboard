52c8b859fea1ea4fc352b587075f49766b7f59cc
Brian Richter <brianorwhatever@gmail.com>
2019-03-28 09:52:54 -0700

    fixed sso path

--- src/app/backup.config.ts
+++ src/app/backup.config.ts
@@ -2,5 +2,5 @@ export const config = {
     NEWS_URL: 'https://dev.news.gov.bc.ca',
     HUB_URL: 'https://dev.hub.gcpe.gov.bc.ca',
     AUTH_TYPE: 'AzureAD',
-    ENV: 'dev'
+    KEYCLOAK_SSO_PREFIX: 'sso-dev'
 };
