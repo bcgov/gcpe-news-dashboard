aff52769e329fb79e07380a698f9aa5771f7d4da
André Lashley <andre.lashley@gov.bc.ca>
2020-03-04 11:48:54 -0800

    Complete upgrade to Angular 9

--- src/app/api.module.ts
+++ src/app/api.module.ts
@@ -18,7 +18,7 @@ export function getApiConfig() {
   providers:    []
 })
 export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
         return {
             ngModule: ApiModule,
             providers: [ { provide: Configuration, useFactory: configurationFactory } ]
