e45183f09193a335f1fed7c9f07e699fa137d96f
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2021-01-15 11:43:10 -0800

    test webhook

--- src/app/backup.config.ts
+++ src/app/backup.config.ts
@@ -1,6 +1,4 @@
 export const config = {
-    NEWS_URL: 'https://dev.news.gov.bc.ca',
-    HUB_URL: 'https://dev.hub.gcpe.gov.bc.ca',
     AUTH_TYPE: 'AzureAD',
     KEYCLOAK_SSO_PREFIX: 'sso-dev',
     LOADING_TIME_EDGE: '12'
