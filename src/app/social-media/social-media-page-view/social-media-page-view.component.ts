0ddb1613de1a0cc937b5fdd5a09da12d1a63c2f4
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2021-09-17 11:48:25 -0700

    new social media layout proposal (#436)
    * upgrade to angular 12
    
    * change to BCSans font
    
    * add BCSans fonts
    
    * fix typo
    
    * fix automated test
    
    * fix automated test
    
    * pull node12 image
    
    * update node version
    
    * pull nodejs 12 image
    
    * upgrade npm version
    
    * fix style minimizer
    
    * revert changes
    
    * disable buildOptimizer
    
    * remove css coments
    
    * resize card font
    
    * resize sub menu font size and margins
    
    * add social media page view
    
    * fix unit test for social media page view
    
    * fix the mobile view
    
    * fix mobile view
    
    * fix mobile view
    
    * remove duplicate script
    
    * fixing mobile view cutoff
    
    * hide sm page view
    
    * new social media layout proposal

--- src/app/social-media/social-media-page-view/social-media-page-view.component.ts
+++ src/app/social-media/social-media-page-view/social-media-page-view.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { SocialMediaRenderService } from '../../services/socialMediaRender.service';
+import { SocialMediaType } from '../../view-models/social-media-type';
+import { SocialMediaPostExtended } from '../../view-models/social-media-post-extended';
+import { Router, ActivatedRoute } from '@angular/router';
+
+const SocialMediaListDivName = 'new-social-media-list';
 
 @Component({
   selector: 'app-social-media-page-view',
   templateUrl: './social-media-page-view.component.html',
   styleUrls: ['./social-media-page-view.component.scss']
 })
-export class SocialMediaPageViewComponent implements OnInit {
+export class SocialMediaPageViewComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor(private socialMediaRenderService: SocialMediaRenderService) { }
+  socialmedia: SocialMediaPostExtended[];
+  selectedSocialMedia: SocialMediaPostExtended[];
+
+  socialmediatypes: SocialMediaType[];
+  filterBySocialMediaType: string;
+
+  isLoading = true;
+
+  constructor(private socialMediaRenderService: SocialMediaRenderService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe(data => {
+      this.socialmedia = data['socialmedia'];
+      this.socialmediatypes = data['socialmediatype'];
+    });
+    console.log(this.socialmedia);
+
+    this.activatedRoute.queryParams.subscribe(() => {
+      this.selectedSocialMedia = this.socialmedia.filter(s => s.mediaType === 'Instagram');
+      this.filterBySocialMediaType = 'Instagram';
+    });
+    console.log('length ' + this.selectedSocialMedia.length);
+  }
+  ngAfterViewInit() {
+    this.selectedSocialMedia.forEach(post => {
+      this.socialMediaRenderService.loadWidgetsWithOptions(post.mediaType, false, SocialMediaListDivName);
+    });
+    this.isLoading = false;
     this.socialMediaRenderService.initFacebook();
     this.socialMediaRenderService.loadTwitterWidgets();
   }
+
+  ngOnDestroy() {
+  }
+
 
 }
